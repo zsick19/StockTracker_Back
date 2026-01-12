9b4b46d78caeec2e83a128cb7d33aa2500339264
zsick19 <39588754+zsick19@users.noreply.github.com>
2026-01-12 10:43:12 -0700

    Added charting schema to ChartableStock to provide ability to add/define a trendline and other charting capabilities.

--- models/ChartableStock.js
+++ models/ChartableStock.js
@@ -32,6 +32,94 @@ const standardDeviationSchema = new mongoose.Schema({
   std2Lower: { type: Number },
 })
 
+const trendLineSchema = new mongoose.Schema({
+  id: { type: Number },
+  dateP1: { type: Date },
+  priceP1: { type: Number },
+  dateP2: { type: Date },
+  priceP2: { type: Number },
+  priceP3: { type: Number },
+  priceP4: { type: Number },
+  dateCreated: { type: Date }
+}, { _id: false })
+const freeLineSchema = new mongoose.Schema({
+  id: { type: Number },
+  dateP1: { type: Date },
+  priceP1: { type: Number },
+  dateP2: { type: Date },
+  priceP2: { type: Number },
+  dateCreated: { type: Date }
+}, { _id: false })
+const lineHSchema = new mongoose.Schema({
+  id: { type: Number },
+  dateP1: { type: Date },
+  priceP1: { type: Number },
+  dateCreated: { type: Date },
+}, { _id: false })
+
+// const channelSchema = new mongoose.Schema({
+//   id: { type: Number },
+//   dateP1: { type: Date },
+//   priceP1: { type: Number },
+//   dateP2: { type: Date },
+//   priceP2: { type: Number },
+//   priceP3: { type: Number },
+//   priceP4: { type: Number },
+//   dateP5: { type: Date },
+//   priceP5: { type: Number },
+//   dateP6: { type: Date },
+//   priceP6: { type: Number },
+//   dateP7: { type: Date },
+//   priceP7: { type: Number },
+//   dateP8: { type: Date },
+//   priceP8: { type: Number },
+//   dateCreated: { type: Date }
+// }, { _id: false })
+// const triangleSchema = new mongoose.Schema({
+//   id: { type: Number },
+//   dateP1: { type: Date },
+//   priceP1: { type: Number },
+//   dateP2: { type: Date },
+//   priceP2: { type: Number },
+//   dateP3: { type: Date },
+//   priceP3: { type: Number },
+//   dateCreated: { type: Date }
+// }, { _id: false })
+// const wedgeSchema = new mongoose.Schema({
+//   id: { type: Number },
+//   dateP1: { type: Date },
+//   priceP1: { type: Number },
+//   dateP2: { type: Date },
+//   priceP2: { type: Number },
+//   priceP3: { type: Number },
+//   priceP4: { type: Number },
+//   dateP5: { type: Date },
+//   priceP5: { type: Number },
+//   dateP6: { type: Date },
+//   priceP6: { type: Number },
+//   dateP7: { type: Date },
+//   priceP7: { type: Number },
+//   dateP8: { type: Date },
+//   priceP8: { type: Number },
+//   dateCreated: { type: Date }
+// }, { _id: false })
+
+//planning schemas
+const enterExitLineSchema = new mongoose.Schema({
+  id: { type: Number },
+  enterDate: { type: Date },
+  enterPrice: { type: Number },
+  enterBufferPrice: { type: Number }, enterBufferPercent: { type: Number },
+  stopLossPrice: { type: Number }, stopLossPercent: { type: Number },
+  exitPrice: { type: Number }, exitPercent: { type: Number },
+  exitBufferPrice: { type: Number }, exitBufferPercent: { type: Number },
+  moonPrice: { type: Number }, moonPercent: { type: Number },
+  riskVRewardIdeal: { type: Number },
+  dateCreated: { type: Date }
+}, { _id: false })
+
+
+
 const chartableStockSchema = new mongoose.Schema({
   tickerSymbol: { type: String, required: true },
   sector: { type: String },
@@ -46,6 +134,22 @@ const chartableStockSchema = new mongoose.Schema({
   putWall: { type: Number },
   useCase: { type: String, immutable: true },
   plannedId: { type: mongoose.Schema.Types.ObjectId, ref: "PlannedStock" },
+  charting: {
+    freeLines: [freeLineSchema],
+    freeLinesId: { type: Number, default: 1 },
+    trendLines: [trendLineSchema],
+    trendLinesId: { type: Number, default: 1 },
+    linesH: [lineHSchema],
+    linesHId: { type: Number, default: 1 },
+    // channels: [channelSchema],
+    // channelsId: { type: Number, default: 1 },
+    // triangles: [triangleSchema],
+    // trianglesId: { type: Number, default: 1 },
+    // wedges: [wedgeSchema],
+    // wedgesId: { type: Number, default: 1 },
+    //enterExitLines: [enterExitLineSchema],
+    //enterExitsId: { type: Number, default: 1 }
+  },
   status: Number,
   dateAdded: { type: Date, default: new Date() },
   chartedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
